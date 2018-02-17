6d91b41aec4e0fdbcf745cc638fb733ac7eedaab
sal <sal2993@hotmail.com>
2018-02-17 11:32:36 -0700

    Made first working contract

--- Contract.sol
+++ Contract.sol
@@ -2,36 +2,32 @@ pragma solidity ^0.4.19;
 
 contract StormBurst {
 
-
+    // mapping (string => Submission) get_submission;
     
-	// Here will lie the future of decentralization
-    mapping (string => Submission) get_submission;
-    
-	//For consistancy with the createSubmition, I've moved the
-	//fields around.
 	struct Submission {
-		string mirror_link;
+		string mirrorLink;
 		string title;
-		//Assuming tag is the key value.
 		string tag;
 	}
 
 	Submission[] public submissions;
 	
-	function _createLink() {};
+	function createSubmission(string _mirrorLink, string _title, string _tag ) public {
+		submissions.push(Submission(_mirrorLink, _title, _tag));
+	}
 
-	//Do you want the submission to immediately be appended to the list?
-	//It seems like each time when the submission is created, we also need to create a key, which maps to the
-	//appended submission.
-	//And what's the tag for?
+	//function getSubmission() {
+	//}
+
+
+
 	//createSubmition (string URI, name, tag) {
-	//	Submission new_submission = Submission(URI, name, tag)
+	//	Submission new_submission = Submission(URI, name, tag) {
 	//	This is where new_submission is "binded" to the tag.
 	//	get_submission[tag] = new_submission
-	//
 	//}
-	
+
 	//getIndex(tag, #){}
-	
+
 	//getIndexList(tag => List of tags, #)
 }
