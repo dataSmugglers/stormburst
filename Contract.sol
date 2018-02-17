5705825b700dd4f76571de7369d12c987ef3d8c4
bob <dlee67@msudenver.edu>
2018-02-17 15:15:17 -0700

    after 3pm

--- Contract.sol
+++ Contract.sol
@@ -2,28 +2,43 @@ pragma solidity ^0.4.18;
 
 contract StormBurst {
 
-	mapping (bytes24 => Submission[] ) public getSubmission;
+	mapping (bytes32 => Submission[] ) public getSubmission;
+	SubmissionList[] public getSubmissionList;
+    Submission[] public submissions;
+    string[] public tag;
 
 	struct Submission {
 		string mirrorLink;
 		string title;
-		bytes24 tag;
+		bytes32 tag;
+	}
+	struct SubmissionList {
+		uint counter;
+		bytes32 tag;
 	}
 
-	Submission[] public submissions;
-
-	function createSubmission(string _mirrorLink, string _title, bytes24 _tag ) public {
-		getSubmission[_tag].push(Submission(_mirrorLink, _title, _tag));
+	function createSubmission(string _mirrorLink, string _title, string _tag) public {
+        saveTag(_tag);
+		bytes32 correctTagType = stringToBytes32(_tag);
+		getSubmission[correctTagType].push(Submission(_mirrorLink, _title, correctTagType));
 	}
 
+    function saveTag(string _tag){
+        tag.push(_tag);
+    }
 
-
-	function getArraySubmission(bytes24 _tag) public view returns(Submission[]) {
-		return (getSubmission[_tag]);
-
+	function getArraySubmission(string _tag) public view returns(Submission[]) {
+	    bytes32 correctTagType = stringToBytes32(_tag);
+		return (getSubmission[correctTagType]);
+    }
+	// Source: https://ethereum.stackexchange.com/questions/9142/how-to-convert-a-string-to-bytes32
+	function stringToBytes32(string memory source) returns (bytes32 result) {
+		bytes memory tempEmptyStringTest = bytes(source);
+		if (tempEmptyStringTest.length == 0) {
+			return 0x0;
+		}
+		assembly {
+			result := mload(add(source, 32))
+		}
 	}
-
-	//getIndex(tag, #){}
-
-	//getIndexList(tag => List of tags, #)
 }
