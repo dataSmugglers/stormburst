01cc3050104c37a64f262ec82fb64d76b6625c1f
bob <dlee67@msudenver.edu>
2018-02-17 01:37:30 -0700

    Bob

--- Contract.sol
+++ Contract.sol
@@ -2,5 +2,13 @@ pragma solidity ^0.4.19;
 
 contract StormBurst {
 
-  // Here will lie the future of decentralization
+	struct Link {
+		string title;
+		string hyper_link;
+	}
+
+	// Here will lie the future of decentralization
+	Link[] public Link;
+	
+	function _createLink() {};
 }
