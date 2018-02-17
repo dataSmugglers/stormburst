7922c9f7e843c8a1b195f4e5c1f6549e90a3fa77
bob <dlee67@msudenver.edu>
2018-02-17 12:20:38 -0700

    after 12PM

--- Contract.sol
+++ Contract.sol
@@ -1,25 +1,32 @@
 pragma solidity ^0.4.19;
-
 contract StormBurst {
 
-    // mapping (string => Submission) get_submission;
-    
+    mapping (string => Submission[]) public get_submission;
+	
 	struct Submission {
 		string mirrorLink;
 		string title;
 		string tag;
 	}
 
-	Submission[] public submissions;
+	//Submission[] public submissions;
 	
-	function createSubmission(string _mirrorLink, string _title, string _tag ) public {
-		submissions.push(Submission(_mirrorLink, _title, _tag));
+	function createSubmission(string _mirrorLink, string _title, string _tag) public {
+		if(get_submission[_tag] == null){	
+			_createNewArray(_mirrorLink, _title, _tag);
+		} else {
+			get_submission[_tag].push(Submission(_mirrorLink, _title, _tag));
+		}
 	}
 
+	function _createNewArray(string _mirrorLink, string _title, string _tag) private {
+		Submission[] public _tagSubmissions;
+		_tagSubmissions.push(Submission(_mirrorLink, _title, _tag));
+		get_submission[_tag] = _tagSubmissions;		
+	}
+	
 	//function getSubmission() {
 	//}
-
-
 
 	//createSubmition (string URI, name, tag) {
 	//	Submission new_submission = Submission(URI, name, tag) {
