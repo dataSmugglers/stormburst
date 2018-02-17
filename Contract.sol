9e013dc034fd01be7eb42f12af2bb5bf8ac4e2e8
sal <sal2993@hotmail.com>
2018-02-17 14:48:38 -0700

    newer version of Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -3,16 +3,22 @@ pragma solidity ^0.4.18;
 contract StormBurst {
 
 	mapping (bytes24 => Submission[] ) public getSubmission;
+	SubmissionList[] public getSubmissionList;
 
 	struct Submission {
 		string mirrorLink;
 		string title;
 		bytes24 tag;
 	}
+	struct SubmissionList {
+		uint counter;
+		bytes24 tag;
+	}
 
 	Submission[] public submissions;
 
 	function createSubmission(string _mirrorLink, string _title, bytes24 _tag ) public {
+
 		getSubmission[_tag].push(Submission(_mirrorLink, _title, _tag));
 	}
 
@@ -22,8 +28,15 @@ contract StormBurst {
 		return (getSubmission[_tag]);
 
 	}
+	// Source: https://ethereum.stackexchange.com/questions/9142/how-to-convert-a-string-to-bytes32
+	//	function stringToBytes32(string memory source) returns (bytes32 result) {
+	//		bytes memory tempEmptyStringTest = bytes(source);
+	//		if (tempEmptyStringTest.length == 0) {
+	//			return 0x0;
+	//		}
 
-	//getIndex(tag, #){}
-
-	//getIndexList(tag => List of tags, #)
+	//		assembly {
+	//			result := mload(add(source, 32))
+	//		}
+	//	}
 }
