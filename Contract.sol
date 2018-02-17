d67e55e3887131b1f063da31598aa55c8da2d7e6
sal <sal2993@hotmail.com>
2018-02-17 02:11:27 -0700

    added like 1 line

--- Contract.sol
+++ Contract.sol
@@ -5,12 +5,19 @@ contract StormBurst {
 
     
   // Here will lie the future of decentralization
-    mapping (string => )
+    mapping (string => submission[]) indecies;
+	mapping (string => ) // This
+
     
 	//Bob
 	struct submission {
 		string title;
 		string mirror_link;
+		string tag;
+	}
+	struct tags {
+		string tag;
+		uint counter;
 	}
 
 	//Bob
@@ -19,7 +26,6 @@ contract StormBurst {
 	//Bob
 	function _createLink() {};
 
-<<<<<<< HEAD
 	//Bob
 	//createSubmition (string URI, ..., name, tag) {}
 	
@@ -28,6 +34,5 @@ contract StormBurst {
 	
 	//Bob
 	//getIndexList(tag => List of tags, #)
-=======
->>>>>>> 222479c8bcb496c6d7c8da65c3fa75bd38a5a4f8
+
 }
