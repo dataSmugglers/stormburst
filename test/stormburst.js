1ff9247df09eda8c89a549ee9fc3ce6c85f66bc9
Luke Smith <lsmith@zenoscave.com>
2018-02-17 19:43:59 -0700

    Testing started
    Signed-off-by: Luke Smith <lsmith@zenoscave.com>

--- test/stormburst.js
+++ test/stormburst.js
@@ -3,10 +3,21 @@ var StormBurst = artifacts.require("./StormBurst.sol");
 contract('StormBurst', function(accounts) {
   it("should allow you to create Submissions", function() {
     return StormBurst.deployed().then(function(instance) {
-      instance.createSubmission('test.xyz', 'test', 'test');
-      return instance.getSubmissionsByTag.call('test');
-    }).then(function(submission) {
-      assert.equals(submission[0].mirrorLink, 'test.xyz', "First Submission not properly saved");
+      return instance.createSubmission("test.xyz/deb8.iso", "Debian 8", "debian").then(function(Success) {
+        return Success;
+      });
+    }).then(function(Success) {
+      assert('tx' in Success, "First Submission not properly saved");
     });
   });
+
+  it("should allow to query tags count", function() {
+    return StormBurst.deployed().then(function(instance) {
+      return instance.createSubmission("test.xyz/deb8.iso", "Debian 8", "debian").then(function(submission) {
+        return instance.getTagCount.call().then(function(count) {return count.toNumber()});  
+      });
+    }).then(function(tagCount) {
+      assert.equal(tagCount, 1, "Tag count query inaccurate");
+    });
+  })
 });
